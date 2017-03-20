672e8cf3fecb86a990ee78f6ee24a3f05829ec6b
Mike Vieira <mikevieira@hotmail.com>
2017-03-20 14:55:33 +0000

    table calendar created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,23 @@
 class TasksController < ApplicationController
 
 def index
+  @tasks = Task.all 
 end 
 
 def new
   @task = Task.new
 end 
 
+def create
+  Task.create(task_params) 
+  redirect_to root_path 
+end 
+
+private
+
+  def task_params
+    params.require(:task).permit(:name, :datetime, :notes)
+  end
+
 end
+
