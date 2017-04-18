61500181641d00b40e75052777514457afc8d6b3
Mike Vieira <mikevieira@hotmail.com>
2017-04-18 14:20:57 +0000

    Task count works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery.timepicker.js
+
 //= require jquery_ujs
 //= require jquery-ui
 //= require bootstrap-sprockets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410022046) do
+ActiveRecord::Schema.define(version: 20170417005758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170410022046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.time     "starttime"
+    t.date     "enddate"
   end
 
 end
